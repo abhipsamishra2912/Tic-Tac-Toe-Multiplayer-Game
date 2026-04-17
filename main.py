7095804a8be5d8e8f757fee8cb294ad17bd88a9f
Abhipsa Mishra <abhipsamishra2007@gmail.com>
2026-04-17 17:56:30 +0000

    correct function calling and db loading

--- main.py
+++ main.py
@@ -1,33 +1,28 @@
 from fastapi import WebSocket, WebSocketDisconnect, FastAPI, Request
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
 from auth import authenticate, init_cache
 from managers.game_manager import GameManager
-from managers.elo_manager import update_ratings
+from managers.elo_manager import update_ratings, get_leaderboard
 from managers.ws_manager import ConnectionManager
-from db import set_user_online
 from managers.match_manager import MatchManager
 from managers.room_manager import RoomManager
-from utils.facial_recognition_module import build_encodings_cache
+from db import set_user_online
 
 app = FastAPI()
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
-    
+
 sessions = {}
-
 match_manager = MatchManager()
-room_manager = RoomManager()
-
-game_manager = GameManager(None, update_ratings, room_manager)
-manager = ConnectionManager(match_manager, room_manager, game_manager)
-
+room_manager  = RoomManager()
+game_manager  = GameManager(None, update_ratings, room_manager)
+manager       = ConnectionManager(match_manager, room_manager, game_manager)
 game_manager.connection_manager = manager
-match_manager.room_manager = room_manager
-match_manager.game_manager = game_manager
+match_manager.room_manager  = room_manager
+match_manager.game_manager  = game_manager
 
 @app.on_event("startup")
-async def startup():
+async def startup_event():
     print("Building face encodings cache...")
     init_cache()
     print("Cache ready")
@@ -35,6 +30,18 @@ async def startup():
 @app.get("/")
 def serve_login():
     return FileResponse("static/login.html")
+
+@app.get("/static/lobby.html")
+def serve_lobby():
+    return FileResponse("static/lobby.html")
+
+@app.get("/static/game.html")
+def serve_game():
+    return FileResponse("static/game.html")
+
+@app.get("/static/leaderboard.html")
+def serve_leaderboard_page():
+    return FileResponse("static/leaderboard.html")
 
 @app.post("/login")
 async def login(request: Request):
@@ -47,7 +54,7 @@ async def login(request: Request):
     if "," in image_data:
         image_data = image_data.split(",", 1)[1]
 
-    uid=await authenticate(image_data)
+    uid = await authenticate(image_data)
 
     if uid is None:
         return {"status": "fail", "message": "Face not recognised"}
@@ -55,11 +62,12 @@ async def login(request: Request):
     sessions[uid] = True
     set_user_online(uid)
 
-    return {
-        "status": "success",
-        "uid": uid,
-        "message": "/static/lobby.html"
-    }
+    return {"status": "success", "uid": uid}
+
+@app.get("/leaderboard")
+async def leaderboard():
+    data = await get_leaderboard()
+    return JSONResponse(data)
 
 @app.websocket("/ws/{uid}")
 async def websocket_endpoint(websocket: WebSocket, uid: str):
@@ -70,8 +78,3 @@ async def websocket_endpoint(websocket: WebSocket, uid: str):
             await manager.handle_message(uid, data)
     except WebSocketDisconnect:
         await manager.disconnect(uid)
-
-@app.get("/leaderboard")
-async def get_db_leaderboard():
-    from managers.elo_manager import get_leaderboard
-    return await get_leaderboard()
